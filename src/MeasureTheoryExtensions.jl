a8ed81c08f6cf0017febaedc96994ebfa6047717
Tim Redick <t.redick@irt.rwth-aachen.de>
2022-04-19 17:10:18 +0200

    Update MeasureTheory

--- src/MeasureTheoryExtensions.jl
+++ src/MeasureTheoryExtensions.jl
@@ -4,6 +4,7 @@
 
 using LogExpFunctions
 using MeasureTheory
+using KeywordCalls
 using Random
 using StatsBase
 using TransformVariables
@@ -16,19 +17,25 @@ f(x; a, b) = \\frac{1}{b - a}, \\quad a \\le x \\le b
 ```
 """
 
-@parameterized UniformInterval(a, b) ≪ Lebesgue(ℝ)
+@parameterized UniformInterval() ≪ Lebesgue(ℝ)
+@kwstruct UniformInterval(a, b)
+UniformInterval(a, b) = UniformInterval((a=a, b=b))
+
+@inline MeasureTheory.insupport(d::UniformInterval{(:a, :b)}, x) = d.a ≤ x ≤ d.b
 
 function MeasureTheory.basemeasure(d::UniformInterval{(:a, :b)})
-    inbounds(x) = d.a <= x <= d.b
     constℓ = 0.0
     varℓ() = -log(d.b - d.a)
     base = Lebesgue(ℝ)
-    FactoredBase(inbounds, constℓ, varℓ, base)
+    FactoredBase(constℓ, varℓ, base)
 end
 
-MeasureTheory.distproxy(d::UniformInterval{(:a, :b)}) = Dists.Uniform(d.a, d.b)
+MeasureTheory.proxy(d::UniformInterval{(:a, :b)}) = Dists.Uniform(d.a, d.b)
 
-MeasureTheory.logdensity(d::UniformInterval{(:a, :b)}, x) = 0.0
+Base.rand(rng::AbstractRNG, ::Type{T}, d::UniformInterval) where {T} = rand(rng, proxy(d))
+
+MeasureTheory.density_def(d::UniformInterval{(:a, :b)}, x) = 1.0
+MeasureTheory.logdensity_def(d::UniformInterval{(:a, :b)}, x) = 0.0
 
 TransformVariables.as(d::UniformInterval{(:a, :b)}) = as(Real, d.a, d.b)
 
@@ -41,18 +48,22 @@ Uses the CircularTransform for continuity.
 """
 
 @parameterized CircularUniform() ≪ Lebesgue(ℝ)
+@kwstruct CircularUniform()
+@inline MeasureTheory.insupport(::CircularUniform, x) = 0 ≤ x ≤ 2π
 
 function MeasureTheory.basemeasure(::CircularUniform{()})
-    inbounds(x) = 0 <= x <= 2π
     constℓ = -log(2π)
     varℓ() = 0.0
     base = Lebesgue(ℝ)
-    FactoredBase(inbounds, constℓ, varℓ, base)
+    FactoredBase(constℓ, varℓ, base)
 end
 
-MeasureTheory.distproxy(::CircularUniform{()}) = Dists.Uniform(0, 2π)
+MeasureTheory.proxy(::CircularUniform{()}) = Dists.Uniform(0, 2π)
 
-MeasureTheory.logdensity(::CircularUniform{()}, x) = 0.0
+Base.rand(rng::AbstractRNG, ::Type{T}, d::CircularUniform) where {T} = rand(rng, proxy(d))
+
+MeasureTheory.density_def(::CircularUniform{(:a, :b)}, x) = 1.0
+MeasureTheory.logdensity_def(::CircularUniform{()}, x) = 0.0
 
 TransformVariables.as(::CircularUniform{()}) = as○
 
@@ -72,9 +83,11 @@ struct MixtureMeasure{T<:Tuple} <: AbstractMeasure
     end
 end
 
+MeasureTheory.insupport(d::MixtureMeasure, x) = reduce(&, d.components)
+
 Base.show(io::IO, d::MixtureMeasure) = print(io, "MixtureMeasure\n  components: [$(d.components)]\n  weights: $(d.weights))")
 
-MeasureTheory.logdensity(μ::MixtureMeasure, x)::Float64 = logsumexp(log(w) + MeasureTheory.logpdf(m, x) for (w, m) in zip(μ.weights, μ.components))
+MeasureTheory.logdensity_def(μ::MixtureMeasure, x)::Float64 = logsumexp(log(w) + MeasureTheory.logdensityof(m, x) for (w, m) in zip(μ.weights, μ.components))
 MeasureTheory.basemeasure(::MixtureMeasure) = Lebesgue(ℝ)
 
 Base.rand(rng::AbstractRNG, T::Type, μ::MixtureMeasure) = rand(rng, T, μ.components[sample(rng, μ.weights)])
@@ -94,9 +107,11 @@ end
 
 BinaryMixture(c1, c2, w1::Number, w2::Number) = BinaryMixture(c1, c2, Float64(w1), Float64(w2))
 
+MeasureTheory.insupport(d::BinaryMixture, x) = insupport(d.c1, x) && insupport(d.c2, x)
+
 Base.show(io::IO, d::BinaryMixture) = print(io, "BinaryMixture\ncomponents: $(d.c1), $(d.c2) \n  log weights: $(d.log_w1), $(d.log_w2)")
 
-MeasureTheory.logdensity(μ::BinaryMixture, x)::Float64 = logaddexp(μ.log_w1 + logpdf(μ.c1, x), μ.log_w2 + logpdf(μ.c2, x))
+MeasureTheory.logdensity_def(μ::BinaryMixture, x)::Float64 = logaddexp(μ.log_w1 + MeasureTheory.logdensityof(μ.c1, x), μ.log_w2 + MeasureTheory.logdensityof(μ.c2, x))
 MeasureTheory.basemeasure(::BinaryMixture) = Lebesgue(ℝ)
 
 function Base.rand(rng::AbstractRNG, T::Type, μ::BinaryMixture)
