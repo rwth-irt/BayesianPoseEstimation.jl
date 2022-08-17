6e6246a68c01dddc27b3006a2bd5c54021f0c88b
Tim Redick <t.redick@irt.rwth-aachen.de>
2022-08-17 09:27:19 +0200

    Rename ImageModel to ObservationModel

--- src/ObservationModel.jl
+++ src/ObservationModel.jl
@@ -7,7 +7,7 @@ using Base: Callable
 using SciGL
 
 """
-    ImageModel(normalize_img, broadcasted_dist, μ)
+    ObservationModel(normalize_img, broadcasted_dist, μ)
 Model to compare rendered and observed depth images.
 During inference it takes care of missing values in the expected depth `μ` and only evaluates the logdensity for pixels with depth 0 < z < max_depth.
 Invalid values of z are set to zero by convention.
@@ -16,7 +16,7 @@ Each pixel is assumed to be independent and the measurement can be described by 
 `μ` is the expected value and `o` is the object association probability.
 Other static parameters should be applied partially to the function beforehand (or worse be hardcoded).
 """
-struct ImageModel{T,N,B<:BroadcastedDistribution{T,N},U<:AbstractArray{T}}
+struct ObservationModel{T,N,B<:BroadcastedDistribution{T,N},U<:AbstractArray{T}}
     normalize_img::Bool
     broadcasted_dist::B
     # Need to access μ to calculate the normalization constant
@@ -24,43 +24,43 @@ struct ImageModel{T,N,B<:BroadcastedDistribution{T,N},U<:AbstractArray{T}}
 end
 
 """
-    ImageModel(normalize_img, pixel_dist, μ, o)
+    ObservationModel(normalize_img, pixel_dist, μ, o)
 Generates a `BroadcastedDistribution` of dim (1,2) from the `pix_dist`, expected depth `μ` and the association probability `o`.
 """
-function ImageModel(normalize_img::Bool, pixel_dist, μ::AbstractArray, o::AbstractArray)
-    broadcasted_dist = BroadcastedDistribution(pixel_dist, Dims(ImageModel), μ, o)
-    ImageModel(normalize_img, broadcasted_dist, μ)
+function ObservationModel(normalize_img::Bool, pixel_dist, μ::AbstractArray, o::AbstractArray)
+    broadcasted_dist = BroadcastedDistribution(pixel_dist, Dims(ObservationModel), μ, o)
+    ObservationModel(normalize_img, broadcasted_dist, μ)
 end
 
 """
-    ImageModel(render_context, scene, object_id, rotation_type, normalize_img, pixel_dist, t, r, o)
-Generate an ImageModel by rendering the expected depth `μ` for the provided scene.
+    ObservationModel(render_context, scene, object_id, rotation_type, normalize_img, pixel_dist, t, r, o)
+Generate an ObservationModel by rendering the expected depth `μ` for the provided scene.
 Sets the pose of `object_id` to the position(s) `t` and orientation(s) `r`.
 """
-function ImageModel(render_context::RenderContext, scene::Scene, object_id::Integer, rotation_type::Type, normalize_img::Bool, pixel_dist, t::AbstractArray, r::AbstractArray, o::AbstractArray)
+function ObservationModel(render_context::RenderContext, scene::Scene, object_id::Integer, rotation_type::Type, normalize_img::Bool, pixel_dist, t::AbstractArray, r::AbstractArray, o::AbstractArray)
     p = to_pose(t, r, rotation_type)
     μ = render(render_context, scene, object_id, p)
-    ImageModel(normalize_img, pixel_dist, μ, o)
+    ObservationModel(normalize_img, pixel_dist, μ, o)
 end
 
 """
-    ImageModel(parameters, render_context, scene, t, r, o)
+    ObservationModel(parameters, render_context, scene, t, r, o)
 Convenience constructor which extracts the `object_id`, `normalize_img`, `rotation_type` and `pixel_dist` from the `parameters` struct.
 Note that `rotation_type` and `pixel_dist` are expected as Symbols which get evaled at runtime. 
 """
-ImageModel(parameters::Parameters, render_context::RenderContext, scene::Scene, t::AbstractArray, r::AbstractArray, o::AbstractArray) = ImageModel(render_context, scene, parameters.object_id, eval(parameters.rotation_type), parameters.normalize_img, eval(parameters.pixel_dist), t, r, o)
+ObservationModel(parameters::Parameters, render_context::RenderContext, scene::Scene, t::AbstractArray, r::AbstractArray, o::AbstractArray) = ObservationModel(render_context, scene, parameters.object_id, eval(parameters.rotation_type), parameters.normalize_img, eval(parameters.pixel_dist), t, r, o)
 
 # Image is always 2D
-const Base.Dims(::Type{<:ImageModel}) = (1, 2)
-const Base.Dims(::ImageModel) = Dims(ImageModel)
+const Base.Dims(::Type{<:ObservationModel}) = (1, 2)
+const Base.Dims(::ObservationModel) = Dims(ObservationModel)
 
 # Generate independent random numbers from m_pix(μ, o)
-Base.rand(rng::AbstractRNG, model::ImageModel, dims::Integer...) = rand(rng, model.broadcasted_dist, dims...)
+Base.rand(rng::AbstractRNG, model::ObservationModel, dims::Integer...) = rand(rng, model.broadcasted_dist, dims...)
 
 # DensityInterface
-@inline DensityKind(::ImageModel) = HasDensity()
+@inline DensityKind(::ObservationModel) = HasDensity()
 
-function DensityInterface.logdensityof(model::ImageModel, x)
+function DensityInterface.logdensityof(model::ObservationModel, x)
     log_p = logdensityof(model.broadcasted_dist, x)
     if model.normalize_img
         # Normalization: divide by the number of rendered pixels
--- test/observation_model.jl
+++ test/observation_model.jl
@@ -51,20 +51,20 @@ logdensityof(pix_dist, x) == logdensityof.(pix_dist, x)
 
 # Single rand
 o = rand(curng, KernelUniform(0.5f0, 1.0f0), 100, 100)
-img_model = ImageModel(true, my_pixel_dist, μ, o)
-img = rand(curng, img_model)
+obs_model = ObservationModel(true, my_pixel_dist, μ, o)
+img = rand(curng, obs_model)
 @test size(img) == (100, 100)
 @test eltype(img) == Float32
 maybe_plot(plot_depth_img, Array(img))
-ℓ = @inferred logdensityof(img_model, img)
+ℓ = @inferred logdensityof(obs_model, img)
 @test ℓ isa Float32
 
 # Multiple rand image model
-img_10 = rand(curng, img_model, 10)
+img_10 = rand(curng, obs_model, 10)
 @test size(img_10) == (100, 100, 10)
 @test eltype(img_10) == Float32
 maybe_plot(plot_depth_img, Array(view(img_10, :, :, 10)))
-ℓ = @inferred logdensityof(img_model, img_10)
+ℓ = @inferred logdensityof(obs_model, img_10)
 @test size(ℓ) == (10,)
 @test ℓ isa CuArray{Float32}
 for layer_id in 1:(size(img_10)[3]-1)
@@ -87,12 +87,12 @@ for layer_id in 1:(size(μ_10)[3]-1)
 end
 
 # Multiple poses & rand image model
-img_model_10 = ImageModel(true, my_pixel_dist, μ_10, o)
-img_10_2 = rand(curng, img_model_10, 2)
+obs_model_10 = ObservationModel(true, my_pixel_dist, μ_10, o)
+img_10_2 = rand(curng, obs_model_10, 2)
 @test size(img_10_2) == (100, 100, 10, 2)
 @test eltype(img_10_2) == Float32
 maybe_plot(plot_depth_img, Array(view(img_10_2, :, :, 10, 2)))
-ℓ = @inferred logdensityof(img_model_10, img_10_2)
+ℓ = @inferred logdensityof(obs_model_10, img_10_2)
 @test size(ℓ) == (10, 2)
 @test ℓ isa CuArray{Float32}
 for layer_id in 1:(size(img_10_2)[3])
@@ -112,24 +112,24 @@ end
 # WARN lots of things have to be right when using eval of params
 params = @set params.rotation_type = QuatRotation
 params = @set params.pixel_dist = my_pixel_dist
-img_model_fn = ImageModel | (params, render_context, scene)
-img_model_pose = img_model_fn(t, r, o)
-@test size(img_model_pose.μ) == (100, 100)
-@test eltype(img_model_pose.μ) == Float32
+obs_model_fn = ObservationModel | (params, render_context, scene)
+obs_model_tro = obs_model_fn(t, r, o)
+@test size(obs_model_tro.μ) == (100, 100)
+@test eltype(obs_model_tro.μ) == Float32
 
 # Single random noise
-img = rand(curng, img_model_pose)
+img = rand(curng, obs_model_tro)
 @test size(img) == (100, 100)
 @test eltype(img) == Float32
 maybe_plot(plot_depth_img, Array(img); clims=(0.0, 2.0))
-ℓ = @inferred logdensityof(img_model_pose, img)
+ℓ = @inferred logdensityof(obs_model_tro, img)
 @test ℓ isa Float32
 
 # Multiple random noise
-img_10 = rand(curng, img_model_pose, 10)
+img_10 = rand(curng, obs_model_tro, 10)
 @test size(img_10) == (100, 100, 10)
 @test eltype(img_10) == Float32
-ℓ = @inferred logdensityof(img_model_pose, img_10)
+ℓ = @inferred logdensityof(obs_model_tro, img_10)
 @test size(ℓ) == (10,)
 @test ℓ isa CuArray{Float32}
 for layer_id in 1:(size(img_10)[3]-1)
@@ -143,14 +143,14 @@ r_dist = BroadcastedDistribution(KernelNormal, [0, 0, 0, 0], [1.0, 1.0, 1.0, 1.0
 T = rand(t_dist, 10)
 R = rand(r_dist, 10)
 
-img_model_pose = img_model_fn(T, R, o)
-@test size(img_model_pose.μ) == (100, 100, 10)
-@test eltype(img_model_pose.μ) == Float32
+obs_model_TRo = obs_model_fn(T, R, o)
+@test size(obs_model_TRo.μ) == (100, 100, 10)
+@test eltype(obs_model_TRo.μ) == Float32
 
-img_10 = rand(curng, img_model_pose)
+img_10 = rand(curng, obs_model_TRo)
 @test size(img_10) == (100, 100, 10)
 @test eltype(img_10) == Float32
-ℓ = @inferred logdensityof(img_model_pose, img_10)
+ℓ = @inferred logdensityof(obs_model_TRo, img_10)
 @test size(ℓ) == (10,)
 @test ℓ isa CuArray{Float32}
 for layer_id in 1:(size(img_10)[3]-1)
@@ -159,10 +159,10 @@ for layer_id in 1:(size(img_10)[3]-1)
 end
 maybe_plot(plot_depth_img, Array(view(img_10, :, :, 4)))
 
-img_10_2 = rand(curng, img_model_pose, 2)
+img_10_2 = rand(curng, obs_model_TRo, 2)
 @test size(img_10_2) == (100, 100, 10, 2)
 @test eltype(img_10_2) == Float32
-ℓ = @inferred logdensityof(img_model_pose, img_10_2)
+ℓ = @inferred logdensityof(obs_model_TRo, img_10_2)
 @test size(ℓ) == (10, 2)
 @test ℓ isa CuArray{Float32}
 for layer_id in 1:(size(img_10_2)[3])
@@ -180,14 +180,14 @@ end
 
 # Multiple associations
 O = rand(curng, KernelUniform(0.0f0, 0.9f0), 100, 100, 10)
-img_model_pose = img_model_fn(t, r, O)
-@test size(img_model_pose.μ) == (100, 100)
-@test eltype(img_model_pose.μ) == Float32
+obs_model_trO = obs_model_fn(t, r, O)
+@test size(obs_model_trO.μ) == (100, 100)
+@test eltype(obs_model_trO.μ) == Float32
 
-img_10 = rand(curng, img_model_pose)
+img_10 = rand(curng, obs_model_trO)
 @test size(img_10) == (100, 100, 10)
 @test eltype(img_10) == Float32
-ℓ = @inferred logdensityof(img_model_pose, img_10)
+ℓ = @inferred logdensityof(obs_model_trO, img_10)
 @test size(ℓ) == (10,)
 @test ℓ isa CuArray{Float32}
 for layer_id in 1:(size(img_10)[3]-1)
@@ -196,10 +196,10 @@ for layer_id in 1:(size(img_10)[3]-1)
 end
 maybe_plot(plot_depth_img, Array(view(img_10, :, :, 5)))
 
-img_10_2 = rand(curng, img_model_pose, 2)
+img_10_2 = rand(curng, obs_model_trO, 2)
 @test size(img_10_2) == (100, 100, 10, 2)
 @test eltype(img_10_2) == Float32
-ℓ = @inferred logdensityof(img_model_pose, img_10_2)
+ℓ = @inferred logdensityof(obs_model_trO, img_10_2)
 @test size(ℓ) == (10, 2)
 @test ℓ isa CuArray{Float32}
 for layer_id in 1:(size(img_10_2)[3])
@@ -217,11 +217,11 @@ end
 
 # Multiple poses & associations
 O = rand(curng, KernelUniform(0.0f0, 0.9f0), 100, 100, 5)
-img_model_pose = img_model_fn(T, R, O)
-@test_throws DimensionMismatch img_10 = rand(curng, img_model_pose)
+obs_model_TRO = obs_model_fn(T, R, O)
+@test_throws DimensionMismatch img_10 = rand(curng, obs_model_TRO)
 
 O = rand(curng, KernelUniform(0.0f0, 0.9f0), 100, 100, 10)
-img_model_pose = img_model_fn(T, R, O)
+img_model_pose = obs_model_fn(T, R, O)
 img_10 = rand(curng, img_model_pose)
 @test size(img_10) == (100, 100, 10)
 @test eltype(img_10) == Float32
