bfceb3b2f56e577f23f4bb335a4a06d7bbfe866f
Tim Redick <t.redick@irt.rwth-aachen.de>
2022-09-05 10:48:21 +0200

    Type stable PosteriorModel

--- src/PosteriorModel.jl
+++ src/PosteriorModel.jl
@@ -8,36 +8,34 @@ using Random
 using Rotations
 using SciGL
 
-# TODO is this the place to get specific about the variable naming? t,r,o,μ,z,...?
 # TODO doc
 
 # TODO Keep it here?
 PriorModel(t_model, r_model, o_model) = IndependentModel((t=t_model, r=r_model, o=o_model))
 
-# TODO Parametric
-struct PosteriorModel
-    # Render related objects
-    render_context
-    scene
-    object_id
-    rotation_type#::T
-    # Probabilistic models
-    prior_model
+struct PosteriorModel{R<:Rotation,P,O,C<:RenderContext,S<:Scene}
+    prior_model::P
     # Expected to be f(μ, o)
-    observation_model
+    observation_model::O
+    # Render related objects
+    render_context::C
+    scene::S
+    object_id::Int
 end
 
-function Base.rand(rng::AbstractRNG, model::PosteriorModel, dims::Integer...)
-    # TODO should not be used during inference, instead rand from the prior.
+PosteriorModel(prior_model::P, observation_model::O, render_context::C, scene::S, object_id::Int, ::Type{R}) where {P,O,C<:RenderContext,S<:Scene,R<:Rotation} = PosteriorModel{R,P,O,C,S}(prior_model, observation_model, render_context, scene, object_id)
+
+function Base.rand(rng::AbstractRNG, model::PosteriorModel{R}, dims::Integer...) where {R}
+    # WARN should not be used during inference, instead rand from the prior.
     prior_sample = rand(rng, model.prior_model, dims...)
-    render_sample = render(model.render_context, model.scene, model.object_id, model.rotation_type, prior_sample)
-    observation_instance = model.observation_model(render_sample.μ, render_sample.o)
+    render_sample = render(model.render_context, model.scene, model.object_id, R, prior_sample)
+    observation_instance = model.observation_model(variables(render_sample).μ, variables(render_sample).o)
     z = rand(rng, observation_instance)
     merge(render_sample, (; z=z))
 end
 
 function render(render_context::RenderContext, scene::Scene, object_id::Integer, rotation_type::Type, sample::Sample)
-    p = to_pose(sample.t, sample.r, rotation_type)
+    p = to_pose(variables(sample).t, variables(sample).r, rotation_type)
     μ = render(render_context, scene, object_id, p)
     # μ is only a view of the render_data. Storing it in every sample is cheap.
     merge(sample, (; μ=μ))
@@ -47,13 +45,14 @@ end
 @inline DensityKind(::PosteriorModel) = HasDensity()
 function DensityInterface.logdensityof(model::PosteriorModel, sample)
     ℓ_prior = logdensityof(model.prior_model, sample)
-    observation_instance = model.observation_model(sample.μ, sample.o)
-    ℓ_likelihood = logdensityof(observation_instance, sample.z)
+    observation_instance = model.observation_model(variables(sample).μ, variables(sample).o)
+    ℓ_likelihood = logdensityof(observation_instance, variables(sample).z)
     .+(promote(ℓ_prior, ℓ_likelihood)...)
 end
 
 # TODO move to it's own file?
 # TODO Parametric
+# TODO Test
 struct RenderProposal#{T<:Rotation}
     # Render related objects
     render_context
