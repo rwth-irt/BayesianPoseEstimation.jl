5239f6840035b5af51c4fd36a26241cf1bcb2047
Tim Redick <t.redick@irt.rwth-aachen.de>
2022-09-27 21:58:38 +0200

    Doc for ComposedSampler. Allow different priors in ComposedSampler

--- src/ComposedSampler.jl
+++ src/ComposedSampler.jl
@@ -13,29 +13,34 @@ using StatsBase
 Provide a different sampler for each group (just like in Turing.jl).
 This way each variable can be sampled with the most appropriate sampler (e.g. using gradients or analytically calculating the conditional posterior).
 
-If the ComposedSampler is `systematic`, the samplers are cycled in order (practical, according to Wikipedia), otherwise a random sampler is selected based on the `weights`.
-Use `weights` for example to sample from the prior only every once in a while which might be inefficient but help with escaping local minima.  
+The samplers are cycled systematically using the sampler indices in `order`.
+You could provide a custom order, e.g. `[1, 2, 2]`, which uses sampler 1 once and then sampler 2 twice.
 """
 struct ComposedSampler{O<:AbstractVector{<:Integer},T<:Tuple} <: AbstractMCMC.AbstractSampler
-    # TODO doc
     order::O
     samplers::T
 end
 
-# TODO doc
-function ComposedSampler(order::AbstractVector{<:Integer}, samplers::AbstractMCMC.AbstractSampler...)
-    if !allequal(prior.(samplers))
-        throw(ArgumentError("ComposedSampler: different priors for samplers not supported since they would require different bijectors"))
-    end
+"""
     ComposedSampler(order, samplers)
-end
-# TODO doc
+Construct a ComposedSampler from an order which can be a vector / range of the sampler indices and an arbitrary number of samplers.
+"""
+ComposedSampler(order::AbstractVector{<:Integer}, samplers::AbstractMCMC.AbstractSampler...) = ComposedSampler(order, samplers)
+
+"""
+    ComposedSampler(order, samplers)
+Construct a ComposedSampler from an arbitrary number of samplers.
+The sampler order is `1:length(samplers)` by default.
+"""
 ComposedSampler(samplers::AbstractMCMC.AbstractSampler...) = ComposedSampler(1:length(samplers), samplers...)
 
-# TODO doc
+"""
+    ComposedSamplerState
+Additionally to the current sample, the current index for cycling the samplers is stored.
+"""
 struct ComposedSamplerState{S<:Sample}
     sample::S
-    i_sampler::Int64
+    index::Int64
 end
 
 function Base.show(io::IO, g::ComposedSampler)
@@ -49,7 +54,7 @@ end
 """
     step(rng, model, sampler)
 Implementing the AbstractMCMC interface for the initial step.
-Proposes one sample from the prior distribution of the model.
+Uses the first sampler to propose the initial sample.
 """
 function AbstractMCMC.step(rng::AbstractRNG, model, sampler::ComposedSampler)
     # All samplers have the same prior
@@ -63,11 +68,12 @@ end
 Implementing the AbstractMCMC interface for steps given a state from the last step.
 """
 function AbstractMCMC.step(rng::AbstractRNG, model, sampler::ComposedSampler, state::ComposedSamplerState)
-    index = sampler.order[state.i_sampler]
+    index = sampler.order[state.index]
     current_sampler = sampler.samplers[index]
     # TODO Different states like multiple weighted samples not supported yet
     sample, _ = AbstractMCMC.step(rng, model, current_sampler, state.sample)
+    # increment sample cycling index, reset to 1 if end is reached
+    new_index = state.index == length(sampler.order) ? 1 : state.index + 1
     # sample, state
-    i_sampler = state.i_sampler == length(sampler.order) ? 1 : state.i_sampler + 1
-    sample, ComposedSamplerState(sample, i_sampler)
+    sample, ComposedSamplerState(sample, new_index)
 end
