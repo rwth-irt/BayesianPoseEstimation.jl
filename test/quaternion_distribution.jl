95ffb85bd3afc1294c67804e0bd7a293faced141
Tim Redick <t.redick@irt.rwth-aachen.de>
2022-10-04 20:05:15 +0200

    Rough draft of plotting spherical densities

--- test/quaternion_distribution.jl
+++ test/quaternion_distribution.jl
@@ -12,9 +12,11 @@ using MCMCDepth
 using Plots
 using Quaternions
 using Random
+using Rotations
+using StaticArrays
 using Test
 
-const PLOT = true
+const PLOT = false
 
 # Yup 42 is bad style
 curng = CUDA.RNG(42)
@@ -86,3 +88,35 @@ if PLOT
     z_angles = atan.(getindex.(vecs, 1), getindex.(vecs, 2))
     histogram(z_angles; proj=:polar, fill=true, nbins=90)
 end
+
+
+R = @inferred to_rotation(rand(rng, circ_dist, 30_000), RotXYZ{Float32})
+# p = SVector{3,}(0, 0, 1.0f0)
+p = SVector{3,}(1.0f0, 0, 0)
+R_p = map(r -> Vector(r * p), R)
+
+plotlyjs()
+R_plot = hcat(R_p...)
+scatter3d(R_plot[1, :], R_plot[2, :], R_plot[3, :]; markersize=1.0, markeralpha=0.3)
+
+using CoordinateTransformations
+plotlyjs()
+sfc = SphericalFromCartesian()
+sph = sfc.(R_p)
+# histogram2d(getproperty.(sph, :θ), getproperty.(sph, :ϕ); nbins=(45, 45))
+
+using StatsBase
+n = 45
+# his = fit(Histogram, getproperty.(sph, :θ), (range(-π, π; length=n), range(-π, π; length=n + 1)))
+# TODO smoother with KDE?
+his = fit(Histogram, (getproperty.(sph, :θ), getproperty.(sph, :ϕ)), (range(-π, π; length=n + 1), range(-π / 2, π / 2; length=n + 1)))
+# heatmap(his.weights')
+
+# Plot histogram on sphere
+using PlotlyJS
+u = range(-π, π; length=n)
+v = range(0, π; length=n)
+x = cos.(u) * sin.(v)'
+y = sin.(u) * sin.(v)'
+z = ones(n) * cos.(v)'
+PlotlyJS.surface(x=x, y=y, z=z, surfacecolor=his.weights) |> PlotlyJS.plot
