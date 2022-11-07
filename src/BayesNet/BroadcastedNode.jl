f76d86f04aa429ea8fff752f842047b5328feeed
Tim Redick <t.redick@irt.rwth-aachen.de>
2022-11-07 21:31:28 +0100

    Test bijectors of BroadcastedNode

--- src/BayesNet/BroadcastedNode.jl
+++ src/BayesNet/BroadcastedNode.jl
@@ -17,6 +17,7 @@ end
 # Convenience constructor for moving name to the parametric type
 BroadcastedNode(name::Symbol, model::M, children::N) where {child_names,M,N<:NamedTuple{child_names}} = BroadcastedNode{name,child_names,M,N}(model, children)
 
+# Construct as parent
 """
     BroadcastedNode(name, dist, children)
 Construct a node which automatically broadcasts the `distribution` over the parameters given by the `children`.
@@ -34,68 +35,9 @@ function BroadcastedNode(name::Symbol, distribution::Type, children::NamedTuple)
 end
 
 # Construct as leaf
-# TODO doc that it is constructed as product distribution
 """
     BroadcastedNode(name, dist, params...)
 Construct the node as leaf (no children) by broadcasting the `distribution` over the `params`.
 The resulting `BroadcastedDistribution` acts like a product distribution, reducing the ndims of the `params`.
 """
 BroadcastedNode(name::Symbol, distribution, params...) = BroadcastedNode(name, ProductBroadcastedDistribution(distribution, params...), (;))
-
-
-# TODO Do I want to enable conversion or should a BroadcastedNode be the default?
-# function broadcast_node(node::AbstractNode{name}) where {name}
-#     variables = rand(node)
-#     traverse(node, (;), variables) do child, _...
-#         broadcast_barrier(child, variables)
-#     end
-# end
-
-# function broadcast_barrier(node::AbstractNode{name}, variables::NamedTuple) where {name}
-#     dims = param_dims(variables[name])
-#     # TODO bake in the dims using partial application
-#     wrapped = BroadcastedDistribution(model(node), dims, x...)
-#     BroadcastedNode(name, broadcasted_model, children(node))
-# end
-
-# BroadcastedNode(name, model::M, children::N) where {child_names,M,N<:NamedTuple{child_names}} = BroadcastedNode{name,child_names,M,N}(model, children)
-
-# # construct as parent
-# """
-#     BroadcastedNode(node, dims)
-# Wraps the model of the `node` with a BroadcastedDistribution of `dims` reduction dimensions.
-# """
-# function BroadcastedNode(node::AbstractNode{name}, dims::Dims) where {name}
-#     broadcasted_model(x...) = BroadcastedDistribution(model(node), dims, x...)
-#     BroadcastedNode(name, broadcasted_model, children(node))
-# end
-
-# # construct as leaf
-# function BroadcastedNode(name::Symbol, model, dims::Dims, args...)
-#     broadcasted_model = BroadcastedDistribution(model, dims, args...)
-#     BroadcastedNode(name, broadcasted_model, (;))
-# end
-
-# """
-#     ProductNode(node)
-# Wraps the model of the `node` with a ProductBroadcastedDistribution which reduces the `dims` of the arguments.
-# """
-# function ProductNode(node::AbstractNode{name}) where {name}
-#     product_model(x...) = ProductBroadcastedDistribution(model(node), x...)
-#     BroadcastedNode(name, product_model, children(node))
-# end
-
-# # construct as leaf
-# function ProductNode(name::Symbol, model, args...)
-#     product_model = ProductBroadcastedDistribution(model, args...)
-#     BroadcastedNode(name, product_model, (;))
-# end
-
-
-# # Do not broadcast, BroadcastedDistribution does this internally
-# (node::BroadcastedNode)(x...) = model(node)(x...)
-# (node::BroadcastedNode{<:Any,()})(x...) = model(node)
-# # Avoid method ambiguities, same as AbstractNode(::NamedTuple)
-# (node::BroadcastedNode)(nt::NamedTuple) = node(childvalues(node, nt)...)
-# (node::BroadcastedNode{<:Any,()})(::NamedTuple) = model(node)
-
--- test/BayesNet/broadcasted.jl
+++ test/BayesNet/broadcasted.jl
@@ -3,9 +3,10 @@
 # All rights reserved. 
 
 # WARN Do not run this if you want Revise to work
-include("../src/MCMCDepth.jl")
+include("../../src/MCMCDepth.jl")
 using .MCMCDepth
 
+using Base: materialize
 using MCMCDepth
 using Random
 using Test
@@ -39,17 +40,30 @@ nt = rand(rng, c)
 b = BroadcastedNode(:b, KernelExponential, fill(1.0f0, 3))
 c = BroadcastedNode(:c, KernelNormal, (; a=a, b=b))
 nt = rand(rng, c, 2)
-@test nt.a isa Array{Float32,2}
-@test size(nt.a) == (3, 2)
-ℓ = @inferred logdensityof(a, nt)
+@test nt.c isa Array{Float32,2}
+@test size(nt.c) == (3, 2)
+ℓ = logdensityof(c, nt)
 @test ℓ isa Array{Float32,1}
 @test size(ℓ) == (2,)
-@test ℓ == [0, 0]
 
-# TODO d = VariableNode(:d, KernelNormal, (; c=c, b=b))
+# Test logdensityof calculation
+d = BroadcastedNode(:d, KernelNormal, (; c=c, b=b))
+nt = rand(rng, d)
+ℓ = logdensityof(d, nt)
+@test ℓ == sum(logdensityof.(KernelUniform(), nt.a) + logdensityof.(KernelExponential(), nt.b) + logdensityof.(KernelNormal.(nt.a, nt.b), nt.c) + logdensityof.(KernelNormal.(nt.c, nt.b), nt.d))
 
-# TEST NOT done yet
-@test ℓ == logdensityof(KernelUniform(), nt.a) + logdensityof(KernelExponential(), nt.b) + logdensityof(KernelNormal(nt.a, nt.b), nt.c) + logdensityof(KernelNormal(nt.c, nt.b), nt.d)
-bij = bijector(c_broad)
-@test bij isa NamedTuple{(:a, :b, :c)}
-@test values(bij) == (bijector(KernelUniform()), bijector(KernelExponential()), bijector(KernelNormal()), bijector(KernelNormal()))
+# Test bijectors
+bij = bijector(d)
+@test bij isa NamedTuple{(:a, :b, :c, :d)}
+a_bij = bijector(ProductBroadcastedDistribution(KernelUniform, 0, fill(1.0f0, 3)))
+@test bij.a.dims == a_bij.dims
+@test materialize(bij.a.bijectors) == materialize(a_bij.bijectors)
+b_bij = bijector(ProductBroadcastedDistribution(KernelExponential, fill(1.0f0, 3)))
+@test bij.b.dims == b_bij.dims
+@test materialize(bij.b.bijectors) == materialize(b_bij.bijectors)
+c_bij = bijector(ProductBroadcastedDistribution(KernelNormal, 0, fill(1.0f0, 3)))
+@test bij.c.dims == c_bij.dims
+@test materialize(bij.c.bijectors) == materialize(c_bij.bijectors)
+d_bij = bijector(ProductBroadcastedDistribution(KernelNormal, 0, fill(1.0f0, 3)))
+@test bij.d.dims == d_bij.dims
+@test materialize(bij.d.bijectors) == materialize(d_bij.bijectors)
