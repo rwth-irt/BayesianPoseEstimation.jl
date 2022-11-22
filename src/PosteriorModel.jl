9e65d9168e92464268694c1b424361d6bd3a3513
Tim Redick <t.redick@irt.rwth-aachen.de>
2022-11-22 22:23:43 +0100

    Port PosteriorModel to BayesNet

--- src/PosteriorModel.jl
+++ src/PosteriorModel.jl
@@ -8,36 +8,36 @@ using DensityInterface
 using Random
 
 """
-    PosteriorModel(data, prior, likelihood)
-Implement the following for `prior` and `likelihood`:
-- `bijector(prior)`
-- `rand(rng, prior, sample, dims...)`
-- `logdensityof(prior, sample)`
-- `logdensityof(likelihood, sample)`
-
+    PosteriorModel(bijectors, data, prior, likelihood)
+Consists of the `prior` and `likelihood` model.
 On construction, the bijectors of the prior are eagerly evaluated, so samples can frequently be generated in the unconstrained domain and evaluated in the model domain - logjac correction included.
 AbstractMCMC expects a model to be conditioned on the data, so it is included here.
 """
-struct PosteriorModel{B<:NamedTuple{<:Any,<:Tuple{Vararg{<:Bijector}}},P,L<:ConditionedModel} <: AbstractMCMC.AbstractModel
+struct PosteriorModel{B<:NamedTuple,D<:NamedTuple,P<:SequentializedGraph,L<:SequentializedGraph} <: AbstractMCMC.AbstractModel
     bijectors::B
+    data::D
     prior::P
     likelihood::L
 end
 
-function PosteriorModel(data::NamedTuple, prior, likelihood)
-    bijectors = map_materialize(bijector(prior))
-    cond_likelihood = ConditionedModel(data, likelihood)
-    PosteriorModel(bijectors, prior, cond_likelihood)
+function PosteriorModel(node::AbstractNode, data::NamedTuple{data_names}) where {data_names}
+    # Only sample variables which are not conditioned on data
+    prior_model = Base.structdiff(prior(node), data)
+    # Eagerly evaluate any lazily broadcasted bijectors
+    bijectors = prior_model |> bijector |> map_materialize
+    # Data conditioned nodes form the likelihood and are not transformed for sampling
+    likelihood_model = sequentialize(node)[data_names]
+    PosteriorModel(bijectors, data, prior_model, likelihood_model)
 end
 
 """
     rand(rng, posterior, dims...)
 Generate a new sample from the prior in the unconstrained domain ℝⁿ.
 """
-function Base.rand(rng::AbstractRNG, posterior::PosteriorModel, dims::Integer...)
-    prior_sample = rand(rng, posterior.prior, dims...)
-    to_unconstrained_domain(prior_sample, posterior.bijectors)
+function Base.rand(posterior::PosteriorModel, dims::Integer...)
     # Generating a sample from the likelihood does only make sense for debugging and would not conform the typical Random interface
+    prior_sample = rand(posterior.prior, dims...) |> Sample
+    to_unconstrained_domain(prior_sample, posterior.bijectors)
 end
 
 # DensityInterface
@@ -48,10 +48,9 @@ Takes care of transforming the sample according to the bijectors of the prior an
 """
 function DensityInterface.logdensityof(posterior::PosteriorModel, sample)
     model_sample, logjac = to_model_domain(sample, posterior.bijectors)
-    ℓ_prior = logdensityof(posterior.prior, model_sample)
-    ℓ_likelihood = logdensityof(posterior.likelihood, model_sample)
-    .+(promote(ℓ_prior, ℓ_likelihood, logjac)...)
+    conditioned_sample = merge(model_sample, posterior.data)
+    ℓ_prior = logdensityof(posterior.prior, variables(conditioned_sample))
+    ℓ_likelihood = logdensityof(posterior.likelihood, variables(conditioned_sample))
+    # add_logdensity to move everything to the CPU if required
+    reduce(add_logdensity, (ℓ_prior, ℓ_likelihood, logjac))
 end
-
-prior(posterior::PosteriorModel) = posterior.prior
-Bijectors.bijector(posterior::PosteriorModel) = posterior.bijectors
