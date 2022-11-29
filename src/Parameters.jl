68583968995932ffa17322b7e4e2d15215ef1579
Tim Redick <t.redick@irt.rwth-aachen.de>
2022-11-29 09:58:21 +0100

    Add association_sigma to Parameters

--- src/Parameters.jl
+++ src/Parameters.jl
@@ -15,47 +15,49 @@ Monolith for storing the parameters.
 Deliberately not strongly typed because the strongly typed struct are constructed in the Main script from this.
 
 # Scene Objects
-* mesh_files: Meshes in the scene
-* object_id: Index of the object to estimate the pose of in the scene
+* `mesh_files` Meshes in the scene
+* `object_id` Index of the object to estimate the pose of in the scene
 
 # Camera
-* width, height: Dimensions of the image.
-* depth: z-dimension resembles the number of parallel renderings
-* f_x, f_y: Focal length of the OpenCV camera calibration
-* c_x, c_y: Optical center of the OpenCV camera calibration
-* min_depth, max_depth: Range limit of the sensor
+* `width, height` Dimensions of the image.
+* `depth` z-dimension resembles the number of parallel renderings
+* `f_x, f_y` Focal length of the OpenCV camera calibration
+* `c_x, c_y` Optical center of the OpenCV camera calibration
+* `min_depth, max_depth` Range limit of the sensor / region of interest
 
 # Observation Model
 ## Sensor Model
-* pixel_σ: Standard deviation of the sensor noise.
-* pixel_θ: Expected value of the exponential distribution → Occlusion expected closer or further away.
+* `pixel_σ` Standard deviation of the sensor noise - typically overestimated.
+* `pixel_θ` Expected value of the exponential distribution → Occlusion expected closer or further away.
 
 ## Object Association
-* prior_o: Constant mixture coefficient for the pixel to object association / probability that a pixel belongs to the object.
+* `association_σ` Standard deviation of the sensor noise used in the pixel association. Typically a magnitude larger than the `pixel_σ`.
+* `prior_o` Constant mixture coefficient for the pixel to object association / probability that a pixel belongs to the object.
+* `proposal_σ_o` Random walk proposals for the association
 
 ## Image Model
-* normalize_img: Normalize the likelihood of an image using the number of rendered pixels. Uses the ValidPixel wrapper.
-* n_normalization_samples: Number of samples to calculate the normalization constant from the expected number of visible pixels.
+* `normalize_img` Normalize the likelihood of an image using the number of rendered pixels. Uses the ValidPixel wrapper.
+* `n_normalization_samples` Number of samples to calculate the normalization constant from the expected number of visible pixels.
 
 # Pose Model
-* mean_t: Mean of the RFID measurement
-* σ_t: Standard deviation of RFID measurement, assumes independent x,y,z components
-## TODO Different rotation models?
+* `mean_t` Mean of the RFID measurement
+* `σ_t` Standard deviation of RFID measurement, assumes independent x,y,z components
 
 # Proposal Model
-* proposal_t: Proposal model for the position (gibbs, independent, symmetric)
-* proposal_σ_t: Standard deviation of the random walk moves for the position
-* proposal_r: Proposal model for the orientation (gibbs, independent, symmetric)
-* proposal_σ_r: Standard deviation of the random walk moves for the orientation
+* `proposal_t` Proposal model for the position (gibbs, independent, symmetric)
+* `proposal_σ_t` Standard deviation of the random walk moves for the position
+* `proposal_r` Proposal model for the orientation (gibbs, independent, symmetric)
+* `proposal_σ_r` Standard deviation of the random walk moves for the Euler angle based orientation
+* `proposal_σ_r_quat` Standard deviation of the random walk moves for the quaternions based orientation
 
 # Inference
-* precision: Type of the floating point precision, typically Float32 (or Float64)
-* device: :CUDA or :CPU which is used in Parameters.array_type, Parameters.rng and Parameters.cpu_rng.
-* seed: Seed of the rng
-* algorithm: Symbol of the inference algorithm
-* n_samples: Number of samples in the chain
-* n_burn_in: Number of samples before recording the chain
-* n_thinning: Record only every n_thinning sample to the chain
+* `precision` Type of the floating point precision, typically Float32 (or Float64)
+* `device` :CUDA or :CPU which is used in Parameters.array_type, Parameters.rng and Parameters.cpu_rng.
+* `seed` Seed of the rng
+* `algorithm` Symbol of the inference algorithm
+* `n_samples` Number of samples in the chain
+* `n_burn_in` Number of samples before recording the chain
+* `n_thinning` Record only every n_thinning sample to the chain
 """
 Base.@kwdef struct Parameters
     # Meshes
@@ -76,7 +78,8 @@ Base.@kwdef struct Parameters
     pixel_θ = 1.0
     mix_exponential = 0.8
     # Pixel association
-    prior_o = 0.6
+    association_σ = 0.2
+    prior_o = 0.5
     proposal_σ_o = 0.01
     # Image Model
     normalize_img = true
@@ -162,6 +165,7 @@ Base.getproperty(p::Parameters, ::Val{:prior_o}) = p.precision.(getfield(p, :pri
 Base.getproperty(p::Parameters, ::Val{:proposal_σ_o}) = p.precision.(getfield(p, :proposal_σ_o))
 Base.getproperty(p::Parameters, ::Val{:normalization_constant}) = p.precision.(getfield(p, :normalization_constant))
 Base.getproperty(p::Parameters, ::Val{:pixel_σ}) = p.precision.(getfield(p, :pixel_σ))
+Base.getproperty(p::Parameters, ::Val{:association_σ}) = p.precision.(getfield(p, :association_σ))
 Base.getproperty(p::Parameters, ::Val{:pixel_θ}) = p.precision.(getfield(p, :pixel_θ))
 Base.getproperty(p::Parameters, ::Val{:mix_exponential}) = p.precision.(getfield(p, :mix_exponential))
 
@@ -170,12 +174,3 @@ Base.getproperty(p::Parameters, ::Val{:σ_t}) = p.precision.(getfield(p, :σ_t))
 Base.getproperty(p::Parameters, ::Val{:proposal_σ_t}) = p.precision.(getfield(p, :proposal_σ_t))
 Base.getproperty(p::Parameters, ::Val{:proposal_σ_r}) = p.precision.(getfield(p, :proposal_σ_r))
 Base.getproperty(p::Parameters, ::Val{:proposal_σ_r_quat}) = p.precision.(getfield(p, :proposal_σ_r_quat))
-
-function Base.getproperty(p::Parameters, ::Val{:association_is})
-    sym = getfield(p, :association_is) |> eval
-    (μ) -> eval(sym){p.precision}(μ, p.pixel_σ)
-end
-function Base.getproperty(p::Parameters, ::Val{:association_not})
-    sym = getfield(p, :association_not)
-    (μ) -> eval(sym){p.precision}(p.pixel_σ)
-end
