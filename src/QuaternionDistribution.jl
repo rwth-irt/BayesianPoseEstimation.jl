4c16b8e679553bbfcc58360536bf33980b897451
Tim Redick <t.redick@irt.rwth-aachen.de>
2022-12-13 16:49:36 +0100

    Add AdditiveQuaternion for simplified compatibility with standard additive proposals

--- src/QuaternionDistribution.jl
+++ src/QuaternionDistribution.jl
@@ -64,11 +64,23 @@ Bijectors.bijector(::QuaternionDistribution) = ZeroIdentity()
 Bijectors.logabsdetjac(::ZeroIdentity, ::Union{Quaternion{T},AbstractArray{<:Quaternion{T}}}) where {T} = zero(T)
 
 """
+    AdditiveQuaternion(q)
+Wraps a quaternion and remaps `+` to `*` and `-` to `/` to support additive proposal interfaces.
+"""
+struct AdditiveQuaternion{T}
+    q::Quaternion{T}
+end
+
+Broadcast.broadcastable(q::AdditiveQuaternion) = Ref(q)
+Base.:+(a::Quaternion, b::AdditiveQuaternion) = robust_normalize(a * b.q)
+Base.:-(a::Quaternion, b::AdditiveQuaternion) = robust_normalize(a / b.q)
+
+"""
     QuaternionPerturbation
 Taylor approximation for small perturbation as described in:
 J. Sola, „Quaternion kinematics for the error-state KF“, Laboratoire dAnalyse et dArchitecture des Systemes-Centre national de la recherche scientifique (LAAS-CNRS), Toulouse, France, Tech. Rep, 2012.
 """
-struct QuaternionPerturbation{T} <: AbstractKernelDistribution{Quaternion{T},Continuous}
+struct QuaternionPerturbation{T} <: AbstractKernelDistribution{AdditiveQuaternion{T},Continuous}
     σ_x::T
     σ_y::T
     σ_z::T
@@ -77,29 +89,9 @@ end
 QuaternionPerturbation(σ=0.01f0::Real) = QuaternionPerturbation(σ, σ, σ)
 
 Distributions.logpdf(dist::QuaternionPerturbation{T}, x::Quaternion) where {T} = logpdf(KernelNormal(zero(T), dist.σ_x), 2 * x.v1) + logpdf(KernelNormal(zero(T), dist.σ_y), 2 * x.v2) + logpdf(KernelNormal(zero(T), dist.σ_z), 2 * x.v3)
+Distributions.logpdf(dist::QuaternionPerturbation, x::AdditiveQuaternion) = logpdf(dist, x.q)
 
-Base.rand(rng::AbstractRNG, dist::QuaternionPerturbation{T}) where {T} = approx_qrotation(rand(rng, KernelNormal(0, dist.σ_x)), rand(rng, KernelNormal(0, dist.σ_y)), rand(rng, KernelNormal(0, dist.σ_z)))
+Base.rand(rng::AbstractRNG, dist::QuaternionPerturbation{T}) where {T} = approx_qrotation(rand(rng, KernelNormal(0, dist.σ_x)), rand(rng, KernelNormal(0, dist.σ_y)), rand(rng, KernelNormal(0, dist.σ_z))) |> AdditiveQuaternion
 
 # Bijectors
 Bijectors.bijector(::QuaternionPerturbation) = ZeroIdentity()
-
-# Proposals
-function propose_quaternion_perturbation(proposal::Proposal, previous_sample, dims...)
-    # Propose in unconstrained domain, perturbation is the quaternion product
-    proposed = map_merge((a, b) -> robust_normalize.(a .* b), previous_sample, rand(proposal.model, dims...))
-    # Evaluate in model domain
-    model_sample, _ = to_model_domain(proposed, proposal.posterior_bijectors)
-    evaluated = evaluate(proposal.evaluation, variables(model_sample))
-    # Sampling in unconstrained domain
-    to_unconstrained_domain(Sample(evaluated), proposal.posterior_bijectors)
-end
-
-# difference is the inverse quaternion product
-function transition_probability_quaternion_perturbation(proposal::Proposal{names}, new_sample, previous_sample) where {names}
-    diff_sample = map_merge((a, b) -> robust_normalize.(a ./ b), new_sample[Val(names)], previous_sample)
-    logdensityof(proposal.model, variables(diff_sample))
-end
-
-quaternion_additive(proposal_model, posterior_model) = Proposal(proposal_model, posterior_model, propose_quaternion_perturbation, transition_probability_quaternion_perturbation)
-
-quaternion_symmetric(proposal_model, posterior_model) = Proposal(proposal_model, posterior_model, propose_quaternion_perturbation, transition_probability_symmetric)
