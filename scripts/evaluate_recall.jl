b90c38153a4ee6ef530028835e9e55587cfe61a7
Tim Redick <t.redick@irt.rwth-aachen.de>
2023-08-24 22:09:23 +0200

    Add recall evaluation script

---
+++ scripts/evaluate_recall.jl
@@ -0,0 +1,40 @@
+# @license BSD-3 https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2023, Institute of Automatic Control - RWTH Aachen University
+# All rights reserved. 
+
+"""
+Load the pose errors from disk, eagerly match the poses and calculate the average recall.
+"""
+
+# TODO enable recall over threshold
+
+using DrWatson
+@quickactivate("MCMCDepth")
+
+using DataFrames
+using PoseErrors
+
+# Combine results by sampler & dataset
+experiment_name = "baseline"
+directory = datadir("exp_pro", experiment_name, "errors")
+results = collect_results(directory)
+function parse_config(path)
+    _, config = parse_savename(path; connector=",")
+    @unpack sampler, dataset, scene_id = config
+    sampler, dataset, scene_id
+end
+transform!(results, :path => ByRow(parse_config) => [:sampler, :dataset, :scene_id])
+
+# Threshold the errors
+transform!(results, :adds => ByRow(x -> threshold_errors(x, 0.1)) => :adds_thresh)
+transform!(results, :vsd => ByRow(x -> threshold_errors(x, 0.3)) => :vsd_thresh)
+transform!(results, :vsdbop => ByRow(x -> threshold_errors(vcat(x...), BOP19_THRESHOLDS)) => :vsdbop_thresh)
+
+# Recall by sampler & dataset
+groups = groupby(results, [:sampler])
+# groups = groupby(results, [:sampler, :dataset])
+recalls = combine(groups, :adds_thresh => (x -> recall(x...)) => :adds_recall, :vsd_thresh => (x -> recall(x...)) => :vsd_recall, :vsdbop_thresh => (x -> recall(x...)) => :vsdbop_recall)
+combine(groups,)
+
+# TODO yeah something seems to be wrong with mtm & mh. They cannot be that much worse
+print(recalls)
