555e233ce57931aea48e0372a29cc49ab74ded5c
Tim Redick <t.redick@irt.rwth-aachen.de>
2023-08-29 22:32:41 +0200

    Commit for experiment

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,9 +4,10 @@
 
 # Run this script from the project root, i.e. mcmc-depth-images folder
 # Execute the script in a background tmux session to avoid stopping it when disconnecting SSH
-julia_remote_nvidia.sh scripts/mcmc_baseline.jl
-julia_remote_nvidia.sh scripts/smc_baseline.jl
-julia_remote_nvidia.sh scripts/evaluate_errors.jl
 julia_remote_nvidia.sh scripts/inference_time.jl
 julia_remote_nvidia.sh scripts/recall_n_steps.jl
 julia_remote_nvidia.sh scripts/recall_n_steps_particles.jl
+
+julia_remote_nvidia.sh scripts/mcmc_baseline.jl
+julia_remote_nvidia.sh scripts/smc_baseline.jl
+julia_remote_nvidia.sh scripts/evaluate_errors.jl
