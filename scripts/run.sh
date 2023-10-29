122f845ce0ba4986242543016860dac55e73d077
Tim Redick <t.redick@irt.rwth-aachen.de>
2023-10-29 13:41:18 +0100

    Do not use startup file in scripts to avoid SpecialFunctionsExt error

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,17 +5,17 @@
 # Run this script from the project root, i.e. mcmc-depth-images folder
 # Execute the script in a background tmux session to avoid stopping it when disconnecting SSH
 
-julia_remote_nvidia.sh scripts/inference_time.jl
-julia_remote_nvidia.sh scripts/smc_mh_resolution.jl
-julia_remote_nvidia.sh scripts/smc_benchmark.jl
-julia_remote_nvidia.sh scripts/mcmc_benchmark.jl
+julia_remote_nvidia.sh --startup-file=no scripts/inference_time.jl
+julia_remote_nvidia.sh --startup-file=no scripts/smc_mh_resolution.jl
+julia_remote_nvidia.sh --startup-file=no scripts/smc_benchmark.jl
+julia_remote_nvidia.sh --startup-file=no scripts/mcmc_benchmark.jl
 
-julia_remote_nvidia.sh scripts/smc_priors.jl
-julia_remote_nvidia.sh scripts/smc_observation.jl
+julia_remote_nvidia.sh --startup-file=no scripts/smc_priors.jl
+julia_remote_nvidia.sh --startup-file=no scripts/smc_observation.jl
 
-julia_remote_nvidia.sh scripts/mcmc_baseline.jl
-julia_remote_nvidia.sh scripts/smc_mh_baseline.jl
-julia_remote_nvidia.sh scripts/evaluate_recall.jl
+julia_remote_nvidia.sh --startup-file=no scripts/mcmc_baseline.jl
+julia_remote_nvidia.sh --startup-file=no scripts/smc_mh_baseline.jl
+julia_remote_nvidia.sh --startup-file=no scripts/evaluate_recall.jl
 
-julia_remote_nvidia.sh scripts/smc_mh_hyperopt.jl
-julia_remote_nvidia.sh scripts/mcmc_mh_hyperopt.jl
+julia_remote_nvidia.sh --startup-file=no scripts/smc_mh_hyperopt.jl
+julia_remote_nvidia.sh --startup-file=no scripts/mcmc_mh_hyperopt.jl
