01a904d5f02df264a20d2ff845ab5353558228b1
Tim Redick <t.redick@irt.rwth-aachen.de>
2023-11-09 23:00:48 +0100

    valdiate mcmc hyperopt for individual parameters

--- scripts/mcmc_baseline_hyperopt.jl
+++ scripts/mcmc_baseline_hyperopt.jl
@@ -3,10 +3,9 @@
 # All rights reserved. 
 
 """
-Run different SMC algorithms on the synthetic BOP datasets:
-* MCMC
-* Forward Proposals
-* Bootstrap
+Run different MCMC algorithms on the synthetic BOP datasets:
+* Metropolis Hastings
+* Multiple Try Metropolis
 
 Model setup for segmentation:
 * Segmentation prior for position `t` and pixel association `o`
@@ -36,27 +35,26 @@ CUDA.allowscalar(false)
 experiment_name = "baseline_hyperopt"
 result_dir = datadir("exp_raw", experiment_name)
 parameters = Parameters()
-@reset parameters.n_particles = 100
-@reset parameters.depth = parameters.n_particles
-sampler = :smc_mh
+sampler = [:mh_sampler, :mtm_sampler]
+# only applicable to MTM
+n_particles = 10
+pose_time = 0.5
 dataset = ["itodd", "lm", "tless"]
 testset = "train_pbr"
-# holdout data
 scene_id = [1:4...]
-configs = dict_list(@dict sampler dataset testset scene_id)
+configs = dict_list(@dict sampler n_particles pose_time dataset testset scene_id)
 
 """
-    rng_posterior_sampler(gl_context, parameters, depth_img, mask_img, mesh, df_row)
+    rng_posterior_sampler(gl_context, parameters, depth_img, mask_img, mesh, df_row, sampler_symbol)
 Assembles the posterior model and the sampler from the loaded images, mesh, and DataFrame row.
 """
-function rng_posterior_sampler(gl_context, parameters, depth_img, mask_img, mesh, df_row)
+function rng_posterior_sampler(gl_context, parameters, depth_img, mask_img, mesh, df_row, sampler_symbol)
     # Context
     cpu_rng = Random.default_rng(parameters)
     dev_rng = device_rng(parameters)
 
     # Setup experiment
     camera = crop_camera(df_row)
-    # Scalar is quite a bit faster than transferring an array back and forth to CUDA
     prior_o = parameters.float_type(0.5)
     # Bias the point prior
     prior_t = df_row.gt_t + rand(KernelNormal(0, first(parameters.σ_t)), 3)
@@ -66,7 +64,7 @@ function rng_posterior_sampler(gl_context, parameters, depth_img, mask_img, mesh
     prior = point_prior(parameters, experiment, cpu_rng)
     posterior = simple_posterior(parameters, experiment, prior, dev_rng)
     # Sampler
-    sampler = smc_mh(cpu_rng, parameters, posterior)
+    sampler = eval(sampler_symbol)(cpu_rng, parameters, posterior)
     # Result
     cpu_rng, posterior, sampler
 end
@@ -75,19 +73,17 @@ end
     timed_inference
 Report the wall time from the point right after the raw data (the image, 3D object models etc.) is loaded.
 """
-function timed_inference(gl_context, parameters, depth_img, mask_img, mesh, df_row)
+function timed_inference(gl_context, parameters, depth_img, mask_img, mesh, df_row, sampler_symbol)
     time = @elapsed begin
-        # Assemble sampler and run inference
-        rng, posterior, sampler = rng_posterior_sampler(gl_context, parameters, depth_img, mask_img, mesh, df_row)
-        states, final_state = smc_inference(rng, posterior, sampler, parameters)
-
+        rng, posterior, sampler = rng_posterior_sampler(gl_context, parameters, depth_img, mask_img, mesh, df_row, sampler_symbol)
+        # Sampling
+        chain = sample(rng, posterior, sampler, parameters.n_steps; discard_initial=parameters.n_burn_in, thinning=parameters.n_thinning, progress=false)
         # Extract best pose and score
-        final_sample = final_state.sample
-        score, idx = findmax(loglikelihood(final_sample))
-        t = variables(final_sample).t[:, idx]
-        r = variables(final_sample).r[idx]
+        score, idx = findmax(loglikelihood.(chain))
+        t = variables(chain[idx]).t
+        r = variables(chain[idx]).r
     end
-    t, r, score, final_state, states, time
+    t, r, score, chain, time
 end
 
 function parse_hyperopt_config(path)
@@ -100,19 +96,23 @@ end
 scene_inference(gl_context, config)
     Save results per scene via DrWatson's produce_or_load for the `config`
 """
-function scene_inference(gl_context, parameters, config)
+function scene_inference(gl_context, config)
     # Extract config and load dataset
-    @unpack scene_id, dataset, testset = config
+    @unpack dataset, testset, scene_id, n_particles, pose_time, sampler = config
+    sampler_symbol = sampler
     scene_df = bop_test_or_train(dataset, testset, scene_id)
+    parameters = Parameters()
+    @reset parameters.n_particles = n_particles
 
     # Load best parameters for dataset
-    pro_df = collect_results(datadir("exp_raw", "smc_mh_hyperopt"))
+    pro_df = collect_results(datadir("exp_raw", "mcmc_mh_hyperopt"))
     transform!(pro_df, :path => ByRow(parse_hyperopt_config) => [:dataset, :model])
     idx = findfirst(x -> x == dataset, pro_df.dataset)
     best_params = best_parameters(pro_df[idx, :].scenario).values
     @reset parameters.o_mask_is = best_params[:o_mask_is]
     @reset parameters.pixel_σ = best_params[:pixel_σ]
     @reset parameters.proposal_σ_r = fill(best_params[:proposal_σ_r], 3)
+    @reset parameters.proposal_σ_t = fill(best_params[:proposal_σ_t], 3)
 
     # Store result in DataFrame. Numerical precision doesn't matter here → Float32
     result_df = select(scene_df, :scene_id, :img_id, :obj_id)
@@ -120,44 +120,41 @@ function scene_inference(gl_context, parameters, config)
     result_df.R = Vector{Quaternion{Float32}}(undef, nrow(result_df))
     result_df.t = Vector{Vector{Float32}}(undef, nrow(result_df))
     result_df.time = Vector{Float32}(undef, nrow(result_df))
-    result_df.final_state = Vector{SmcState}(undef, nrow(result_df))
-    result_df.log_evidence = Vector{Vector{Float32}}(undef, nrow(result_df))
+    result_df.chain = Vector{Vector{Sample}}(undef, nrow(result_df))
 
     # Benchmark model sampler configuration to adapt number of steps - also avoids timing pre-compilation
     df_row = first(scene_df)
     depth_img, mask_img, mesh = load_img_mesh(df_row, parameters, gl_context)
-    rng, posterior, sampler = rng_posterior_sampler(gl_context, parameters, depth_img, mask_img, mesh, df_row)
+    rng, posterior, sampler = rng_posterior_sampler(gl_context, parameters, depth_img, mask_img, mesh, df_row, sampler_symbol)
     step_time = mean_step_time(rng, posterior, sampler)
-    @reset parameters.n_steps = floor(Int, parameters.time_budget / step_time)
+    @reset parameters.n_steps = floor(Int, pose_time / step_time)
     # For slow systems
     if parameters.n_steps < 2
         @reset parameters.n_steps = 2
     end
 
     # Run inference per detection
-    @progress "scene_id: $scene_id" for (idx, df_row) in enumerate(eachrow(scene_df))
+    @progress "sampler: $(sampler_symbol), scene_id: $scene_id" for (idx, df_row) in enumerate(eachrow(scene_df))
         # Image crops differ per object
         depth_img, mask_img, mesh = load_img_mesh(df_row, parameters, gl_context)
         # Run and collect results
-        t, R, score, final_state, states, time = timed_inference(gl_context, parameters, depth_img, mask_img, mesh, df_row)
-        # Avoid too large files by only saving t, r, and the logevidence not the sequence of states
-        final_state = collect_variables(final_state, (:t, :r))
-        # Avoid out of GPU errors
-        @reset final_state.sample = to_cpu(final_state.sample)
+        t, R, score, chain, time = timed_inference(gl_context, parameters, depth_img, mask_img, mesh, df_row, sampler_symbol)
+        # Avoid out of GPU errors and save storage by not storing μ and o
+        chain = collect_variables(chain, (:t, :r))
         result_df[idx, :].score = score
         result_df[idx, :].R = R
         result_df[idx, :].t = t
         result_df[idx, :].time = time
-        result_df[idx, :].final_state = final_state
-        result_df[idx, :].log_evidence = logevidence.(states)
+        result_df[idx, :].chain = chain
     end
+    # Return result
     @strdict parameters result_df
 end
 
 # Avoid recreating the context in scene_inference by conditioning on it / closure
 gl_context = render_context(parameters)
-gl_scene_inference = scene_inference | (gl_context, parameters)
-@progress "SMC baseline" for config in configs
+gl_scene_inference = scene_inference | gl_context
+@progress "MCMC baseline" for config in configs
     @produce_or_load(gl_scene_inference, config, result_dir; filename=my_savename)
 end
 destroy_context(gl_context)
